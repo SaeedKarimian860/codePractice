ff7e27d84a65e59bd792e77ce21a039544d24d01
SaeedKarimian860 <SGK12190@gmail.com>
2024-12-31 21:02:38 -0500

    JavaScript Practice

--- Logical Operators/app.js
+++ Logical Operators/app.js
@@ -6,13 +6,26 @@
 //     console.log('Invalid format for password!');
 // }
 
-const age = 22;
-if ((age >= 0 && age < 5) || age >= 65) {
-  console.log("Free");
-} else if (age >= 5 && age < 10) {
-  console.log("$10");
-} else if (age >= 10 && age < 65) {
-  console.log("$20");
-} else {
-  console.log("Invalid Age!!");
+// const age = 22;
+// if ((age >= 0 && age < 5) || age >= 65) {
+//   console.log("Free");
+// } else if (age >= 5 && age < 10) {
+//   console.log("$10");
+// } else if (age >= 10 && age < 65) {
+//   console.log("$20");
+// } else {
+//   console.log("Invalid Age!!");
+// }
+
+const age = 8;
+if (!(age >= 0 && age < 5 || age >= 65)) {
+  console.log("You are not eligible for a free pass!");
 }
+
+
+
+
+const firstName = prompt("enter your first name");
+  if (!firstName) {
+    firstName = prompt("Try Again!!");
+  }
