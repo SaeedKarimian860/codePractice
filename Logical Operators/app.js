b959ecb51c7742452603865ef0d1d527913b628a
SaeedKarimian860 <SGK12190@gmail.com>
2024-12-31 21:19:34 -0500

    JavaScript Practice

--- Logical Operators/app.js
+++ Logical Operators/app.js
@@ -23,9 +23,33 @@ if (!(age >= 0 && age < 5 || age >= 65)) {
 }
 
 
-
-
 const firstName = prompt("enter your first name");
   if (!firstName) {
     firstName = prompt("Try Again!!");
   }
+
+
+// Switch statements
+const day = 2;
+switch(day) {
+  case 1: 
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3: 
+    console.log("Wednesday");
+    break;
+  case 4: 
+    console.log("Thursday");
+    break;
+  case 5: 
+    console.log("Friday");
+    break;
+    case 6:
+    case 7:
+      console.log("Weekend!");
+  default: 
+    console.log("I am not sure!")
+}
