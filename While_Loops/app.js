dd586416e87dec71f1b0503264eb837cba41e211
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-02 23:54:28 -0500

    JavaScript Practice

--- While_Loops/app.js
+++ While_Loops/app.js
@@ -4,10 +4,40 @@
 //     console.log(count);
 // }
 
-const SECRET = "BabyHippo";
+// const SECRET = "BabyHippo";
 
-let guess = prompt ("Enter the secret code...");
-while (guess !== SECRET) {
-    guess = prompt("enter the secret code...");
+// let guess = prompt ("Enter the secret code...");
+// while (guess !== SECRET) {
+//     guess = prompt("enter the secret code...");
+// }
+// console.log("Congrats you got the secret!!!")
+
+
+
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+  maximum = parseInt(prompt("Enter a valid number!"));
 }
-console.log("Congrats you got the secret!!!")
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter you first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+  attempts++;
+  if (guess > targetNum) {
+    guess = prompt("Too high!! Entera new guess:");
+  } else {
+    guess = prompt("Too low! Enter a new guess:");
+  }
+}
+if (guess === 'q') {
+    console.log("Ok, You Quit!!")
+} else {
+    console.log("Congrats! You Win!!!!")
+    console.log(`You Got it!!! It took you ${attempts} guesses`);
+}
