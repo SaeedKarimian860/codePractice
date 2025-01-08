3228b8c9daa1a5e2e3b3fd02fa3c1d147263a781
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-07 22:47:36 -0500

    JavaScript Practice

---
+++ FunctionMethods/app.js
@@ -0,0 +1,21 @@
+// const myMath = {
+//     PI: 3.14159,
+//     square: function(num) {
+//         return num * num;
+//     },
+//     cube: function(num) {
+//         return num ** 3;
+//     }
+// }
+
+
+//Shorthand Method
+const myMath = {
+    PI: 3.14159,
+    square(num) {
+        return num * num;
+    },
+    cube(num) {
+        return num ** 3;
+    }
+}
