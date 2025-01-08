fbb5e46706f7e312a0d9772d589315b5c68be53a
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-07 22:05:16 -0500

    JavaScript Practice

--- FunctionExpressions/app.js
+++ FunctionExpressions/app.js
@@ -13,21 +13,58 @@ const add = function(x, y) {
 
 //Higher Order Functions
 // - Can pass functions as arguments
-// - Can return a function
-function callTwice(func) {
-    func();
-    func();
-}
+// function callTwice(func) {
+//     func();
+//     func();
+// }
 
-function callTenTimes(f) {
-    for (let i = 0; i < 10; i++) {
-        f();
+// function callTenTimes(f) {
+//     for (let i = 0; i < 10; i++) {
+//         f();
+//     }
+// }
+
+// function rollDie() {
+//     const roll = Math.floor(Math.random() * 6) + 1;
+//     console.log(roll);
+// }
+
+// callTwice(rollDie);
+
+
+// - Can return a function
+function makeMysteryFunc() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function() {
+            console.log("Congrats, I am a good function! ");
+            console.log("You Win!!");
+        }
+    } else {
+        return function() {
+            alert("You have been infected by a Computer Virus!");
+            alert("Stop trying to close this Window!!!");
+            alert("Stop trying to close this Window!!!");
+            alert("Stop trying to close this Window!!!");
+            alert("Stop trying to close this Window!!!");
+            alert("Stop trying to close this Window!!!");
+        }
     }
 }
 
-function rollDie() {
-    const roll = Math.floor(Math.random() * 6) + 1;
-    console.log(roll);
+
+
+function makeBetweenFunc(min, max) {
+    return function(num) {
+        return num >= min && num <= max;
+    }
 }
 
-callTwice(rollDie);
+// makeBetweenFunc(50, 100);
+
+// function isBetween(num) {
+//     return num >= 50 && num <= 100;
+//     }
+// function isBetween2(num) {
+//     return num >= 1 && num <= 10;
+// }
