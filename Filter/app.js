766f7fa92ef3f0f79d8f43bee40de42ec5b32201
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-09 23:30:33 -0500

    JavaScript Practice

---
+++ Filter/app.js
@@ -0,0 +1,50 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+numbers.filter(n => {
+    return n === 4
+})
+
+// numbers.filter(n => {
+//     return n < 10
+// })
+
+
+const movies = [
+    {
+        title: 'Men in Black',
+        score: 90,
+        year: 1997
+    },
+    {
+        title: 'A Clockwork Orange',
+        score: 88,
+        year: 1972 
+    },
+    {
+        title: 'Mean Machine',
+        score: 80,
+        year: 2001
+    },
+    {
+        title: 'A Fistful of Dollars',
+        score: 98,
+        year: 1964
+    },
+    {
+        title: 'Master of the Flying Guillotine',
+        score: 78,
+        year: 1976
+    }
+]
+
+// const goodMovies = movies.filter(movie => {
+//     return movies.score > 80
+// })
+
+//using implicit one-liner
+const goodMovies = movies.filter(m => m.score > 80)
+const goodTitles = goodMovies.map(m > m.title)
+
+movies.filter(m => m.score > 80).map(m => m.title)
+const badMovies = movies.filter(m => m.score < 80)
+
