ef23dd9ecc4d14aa333994b4d06d77c7c50b95ed
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-11 18:53:17 -0500

    JavaScript Practice

--- Reduce/app.js
+++ Reduce/app.js
@@ -1,4 +1,4 @@
-const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+const prices = [9.99, 1.5, 19.99, 49.99, 30.5];
 
 //using for...of
 // let total = 0;
@@ -7,16 +7,49 @@ const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
 // }
 // console.log(total);
 
-
 //using the reduce method
 const total = prices.reduce((total, price) => {
-    return total + price;
+  return total + price;
 });
 
-
 const minPrice = prices.reduce((min, price) => {
-    if (price < min) {
-        return price;
+  if (price < min) {
+    return price;
+  }
+  return min;
+});
+
+const movies = [
+  {
+    title: "Men in Black",
+    score: 90,
+    year: 1997,
+  },
+  {
+    title: "A Clockwork Orange",
+    score: 88,
+    year: 1972,
+  },
+  {
+    title: "Mean Machine",
+    score: 80,
+    year: 2001,
+  },
+  {
+    title: "A Fistful of Dollars",
+    score: 98,
+    year: 1964,
+  },
+  {
+    title: "Master of the Flying Guillotine",
+    score: 78,
+    year: 1976,
+  },
+];
+
+const highestRated = movies.reduce((bestMovie, currMovie) => {
+    if (currMovie.score > bestMovie.score) {
+        return currMovie;
     }
-    return min;
+    return bestMovie;
 });
